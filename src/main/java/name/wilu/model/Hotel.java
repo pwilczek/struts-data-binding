3771fbbf76eb4b690f1f5118f9376491d3daa71a
Piotr Wilczek <piotr.wilczek@wilu.name>
2015-01-26 21:05:50 +0100

    Added Hotel entity.

---
+++ src/main/java/name/wilu/model/Hotel.java
@@ -0,0 +1,22 @@
+package name.wilu.model;
+
+public class Hotel {
+
+    private String name, city;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
